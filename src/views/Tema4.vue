da6689fd98d10faca0582d812c86fd346eb70956
Sebastian Trujillo <129899570+strujilloa@users.noreply.github.com>
2023-11-17 16:32:58 -0500

    Update tema4

--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -7,17 +7,246 @@
         span 4
       h1 Evaluación
     
+    .row.justify.content-center.align-items-center 
+      .col-lg-6.col-12
+        .cajon.p-4(style="background-color: #E9FFFA")
+          .row.justify-content-center
+            .col-lg-10.col-12
+              p(data-aos="fade-left") La evaluación formativa es una modalidad de evaluación que se lleva a cabo durante el proceso de enseñanza-aprendizaje con el propósito de detectar las dificultades y los progresos de los estudiantes. Su objetivo es empoderar a los estudiantes al darles la iniciativa de conducir su propio proceso de aprendizaje y profundizar su conocimiento. Esto se logra al desarrollar competencias metacognitivas, lo que implica que cada estudiante identifique el método de aprendizaje que mejor le funcione, autorregule su proceso de aprendizaje y comprenda los procesos involucrados en el aprendizaje.
+      .col-lg-6.col-12
+        figure
+          img(src='@/assets/curso/temas/tema4/1.jpg', alt='imagen decorativa')
+
+    .row.mt-5
+      p(data-aos="fade-left") A continuación, se presentan algunas características de la evaluación formativa que pueden ser útiles en su entorno de trabajo y que contribuirán a la creación de espacios de aprendizaje significativo con la comunidad con la que esté trabajando:
+
+    .row.justify-content-center.align-items-center.mt-5
+      .col-lg-2.col-md-4.col-12
+        figure
+          img(src='@/assets/curso/temas/tema4/2.svg', alt='imagen decorativa')
+      .col-lg-8.col-12
+        .cajon.p-4(style="background-color: #EEFFDE")
+          .row.justify-content-center 
+            .col-lg-11.col-12
+              ol.lista-ol--cuadro.lista-ol
+                li 
+                  .lista-ol--cuadro__vineta
+                    span a
+                  | Es aquella que centra su intervención en los procesos de mejora.
+                li 
+                  .lista-ol--cuadro__vineta
+                    span b
+                  | Su función es orientadora, reguladora y motivadora.
+                li 
+                  .lista-ol--cuadro__vineta
+                    span c
+                  | Se utiliza preferiblemente para ajustar sobre la marcha, los procesos educativos en miras de conseguir las metas previstas.
+                li 
+                  .lista-ol--cuadro__vineta
+                    span d
+                  | Suele identificarse con la evaluación continua porque permite obtener información sobre el desarrollo del proceso educativo de todos los aprendices.
+                li 
+                  .lista-ol--cuadro__vineta
+                    span e
+                  | Permite ajustar o reforzar ciertas acciones y tener claridad sobre la trayectoria de aprendizaje.
+                li 
+                  .lista-ol--cuadro__vineta
+                    span f
+                  | La información que aporta debe dar herramientas tanto a los aprendices como a los instructores.
+    
+    p.mt-5.mb-5(data-aos="fade-left") Es fundamental discutir los tipos de técnicas utilizadas en los procesos de evaluación educativa, ya que son herramientas clave para identificar las áreas específicas donde los estudiantes pueden tener dificultades en su aprendizaje. Estas técnicas permiten a los educadores generar planes de mejora continua y evaluar si la comunidad estudiantil ha alcanzado los objetivos definidos. A continuación, se enumeran algunas de estas técnicas:
+
+    .row.justify-content-center.align-items-center 
+      .col-lg-4.col-md-6.col-12
+        figure
+          img(src='@/assets/curso/temas/tema4/3.jpg', alt='imagen decorativa')
+      .col-lg-6.col-12
+        .cajon.p-4(style="background-color: #E6FFF9")
+          .row.justify-content-center 
+            .col-lg-10
+              ul.lista-ul
+                li 
+                  i.fas.fa-angle-right(style="color: #FFAE7E")
+                  | Análisis de prácticas de instructores.
+                li 
+                  i.fas.fa-angle-right(style="color: #FFAE7E")
+                  | Ensayos libres.
+                li 
+                  i.fas.fa-angle-right(style="color: #FFAE7E")
+                  | Encuestas.
+                li 
+                  i.fas.fa-angle-right(style="color: #FFAE7E")
+                  | Observación directa.
+                li 
+                  i.fas.fa-angle-right(style="color: #FFAE7E")
+                  | Corrección de actividades.
+                li 
+                  i.fas.fa-angle-right(style="color: #FFAE7E")
+                  | Elaboración de proyectos.
+                li 
+                  i.fas.fa-angle-right(style="color: #FFAE7E")
+                  | Desarrollo de técnicas de estudio.
+                li 
+                  i.fas.fa-angle-right(style="color: #FFAE7E")
+                  | Listas de control.
+                li 
+                  i.fas.fa-angle-right(style="color: #FFAE7E")
+                  | Cuestionarios.
+
+    p.mt-5(data-aos="fade-left") Estas técnicas son valiosas para la evaluación formativa, que tiene como objetivo mejorar el proceso de enseñanza-aprendizaje y guiar a los estudiantes hacia un aprendizaje más efectivo y significativo.
+
+
     separador 
     .titulo-segundo.color-acento-contenido
       h2#t_4_1 4.1 Criterios de evaluación
 
+    p(data-aos="fade-left") Los criterios de evaluación son el referente específico para evaluar el aprendizaje de los aprendices, ya que describen lo que se quiere valorar y lo que se espera que el alumno logre, tanto en competencias como en conocimiento.
+    p.mb-5(data-aos="fade-left") Las especificaciones en los criterios de evaluación permiten definir los resultados de aprendizaje y concretar lo que el estudiante debe saber hacer y comprender en dicha asignatura, estos deben ser medibles, observables y evaluables.
+
+    .row 
+      .tarjeta--container.row.mb-5
+        .col-md.tarjeta.p-5(style="background-color: #C1E29A")
+          .row.justify-content-center.mb-4
+            .col-6
+              figure
+                img(src='@/assets/curso/temas/tema4/4.svg', alt='Texto que describa la imagen')
+          
+          h4.text-center De acuerdo con su origen
+          ul.lista-ul.text-center
+            li 
+              i.lista-ul__vineta
+              | Personales
+            li 
+              i.lista-ul__vineta
+              | Disciplinarios
+            li 
+              i.lista-ul__vineta
+              | De la institución educacional
+
+        .col-md.tarjeta.p-5(style="background-color: #D5FFF5")
+          .row.justify-content-center.mb-4
+            .col-6
+              figure
+                img(src='@/assets/curso/temas/tema4/5.svg', alt='Texto que describa la imagen')
+          
+          h4.text-center Según la forma de usar los criterios para juzgar
+          ul.lista-ul.text-center
+            li 
+              i.lista-ul__vineta
+              | Analíticos
+            li 
+              i.lista-ul__vineta
+              | Holísticos
+
+        .col-md.tarjeta.p-5(style="background-color: #FFAE7E")
+          .row.justify-content-center.mb-4
+            .col-6
+              figure
+                img(src='@/assets/curso/temas/tema4/6.svg', alt='Texto que describa la imagen')
+          
+          h4.text-center Con base en el momento de aplicación de la situación de evaluación
+          ul.lista-ul.text-center
+            li 
+              i.lista-ul__vineta
+              | Antes
+            li 
+              i.lista-ul__vineta
+              | Después
+
     separador 
     .titulo-segundo.color-acento-contenido
       h2#t_4_2 4.2 Medición de resultados
+    
+    p(data-aos="fade-left") La medición del aprendizaje es una práctica fundamental en educación que ha experimentado un aumento significativo en los últimos años. Estos procesos se llevan a cabo para evaluar el alcance de los logros de aprendizaje y determinar si se han cumplido las expectativas a lo largo del tiempo. Las mediciones de resultados permiten realizar comparaciones y obtener conclusiones sobre el proceso de adquisición de competencias por parte de un grupo o comunidad a lo largo del tiempo.
+    p.mb-5(data-aos="fade-left") Las funciones de la medición del aprendizaje son las siguientes:
+
+    .row.justify-content-center
+      .col-lg-6.col-12.mb-3
+        .tarjeta.p-4(style="background-color: #E6FFF9; height: 100%")
+          h4 Obtener información para la toma de decisiones:
+          p estas mediciones proporcionan información valiosa que impacta en la toma de decisiones para mejorar el aprendizaje de los estudiantes o del grupo en su conjunto.
+      .col-lg-6.col-12.mb-3
+        .tarjeta.p-4(style="background-color: #E6FFF9; height: 100%")
+          h4 Identificar el desempeño del estudiante:
+          p ayudan a evaluar el desempeño de los estudiantes en relación con los criterios y estándares establecidos.
+      .col-lg-6.col-12.mb-3
+        .tarjeta.p-4(style="background-color: #E6FFF9; height: 100%")
+          h4 Trabajar dentro de criterios claros:
+          p las mediciones se realizan dentro de criterios claros, conocidos y predefinidos, lo que garantiza que la evaluación sea justa y objetiva.
+      .col-lg-6.col-12.mb-3
+        .tarjeta.p-4(style="background-color: #E6FFF9; height: 100%")
+          h4 Comunicar a los estudiantes:
+          p la comunicación de los resultados de las mediciones a los estudiantes les permite comprender su proceso y resultados, lo que a su vez puede contribuir a mejorar su aprendizaje.
+    
+    .row.mt-5
+      p(data-aos="fade-left") En cuanto a las fórmulas para evaluar lo que aprenden los estudiantes, existen hasta seis enfoques, y su elección depende del objetivo y el momento de la evaluación. A continuación, se explicará en qué consiste cada una de ellas:
+
+    .row.mt-5
+      SlyderB.color-acento-botones.mb-5.p-5(:datos="datosSlyder")(style="background-color:#EEFFDE")
 
     separador 
     .titulo-segundo.color-acento-contenido
       h2#t_4_3 4.3 Técnicas recolección de información
+    
+    p(data-aos="fade-left") Es esencial considerar las fuentes de información y las técnicas de recolección de datos en el proceso educativo, ya que estas fuentes y técnicas respaldan y fundamentan el proceso de enseñanza-aprendizaje. La elección y utilización adecuada de estas fuentes son fundamentales para asegurarse de que los resultados obtenidos sean veraces y confiables.
+
+    .row.justify-content-center.align-items-center 
+      .col-lg-8.col-12.mb-3
+        p(data-aos="fade-left") Las fuentes de información pueden variar en su origen y en la forma en que se recopilan. Es importante seleccionar fuentes confiables que respalden la información que se presenta en el proceso de enseñanza. Además, es crucial comprender cómo interactúan las diferentes fuentes de información, ya que esto puede variar según el tema y el nivel de investigación.
+        p(data-aos="fade-left") Cuando trabaja con la comunidad en temas de concienciación del cuidado, saneamiento y salud ambiental, es especialmente importante utilizar información de primera mano para que sea relevante y específica para la población en cuestión.
+        p(data-aos="fade-left") Los instrumentos de recolección de información desempeñan un papel crucial en la investigación, ya que permiten recopilar datos directos de la población o el lugar de interés que se está investigando. Estos instrumentos deben ser sistemáticos y organizados, lo que garantiza la confiabilidad de los datos recopilados y facilita su posterior análisis.
+      .col-lg-4.col-md-6.col-8.mb-3
+        figure
+          img(src='@/assets/curso/temas/tema4/13.png', alt='imagen decorativa')
+    p.mt-5(data-aos="fade-left") La información es un recurso valioso en cualquier investigación, y la toma de decisiones informadas depende en gran medida de la calidad y veracidad de la información recopilada. Por lo tanto, contar con información suficiente y veraz es fundamental para tomar decisiones fundamentadas y efectivas que beneficien a la comunidad.
+
+    .row.justify-content-center.align-items-center.mb-3
+      .col-lg-10.col-12
+        .tarjeta-avatar-b.mb-5
+          .tarjeta-avatar-b__img            
+            img(src='@/assets/curso/temas/img01.svg' alt='AvatarTop')
+          .tarjeta(style="background-color:#FFEBDF")
+            .p-4
+            p.p-2 Los métodos de recolección de información posibilitan que las organizaciones puedan responder a preguntas importantes para el desarrollo de sus actividades, que puedan evaluar los resultados y que con estos datos obtenidos se puedan adelantar a futuras tendencias y anticipar las probabilidades.
+
+    p(data-aos="fade-left") Estos métodos van desde las actas de incidencias o las evaluaciones de competencias del personal, hasta las auditorías e inspecciones y son de gran utilidad para que la empresa pueda avanzar y mejorar su funcionamiento con cada nueva información obtenida.
+    p(data-aos="fade-left") A continuación, se mencionan las principales técnicas de recolección de información que le servirán en su vida laboral, para recolectar información de los grupos de trabajo o las comunidades que esté interviniendo como promotor ambiental:
+
+    .row.justify-content-center
+      .col-lg-10.col-12
+        .titulo-sexto.color-acento-contenido
+          h5 Tabla 2.
+          span #[i Técnicas recolección información]
+        .tabla-a.color-acento-contenido.mb-5 
+          table
+            thead
+              tr(style="background-color: #FFAE7E")
+                th Técnicas
+                th Concepto
+            tbody
+              tr
+                td.text-center Entrevista
+                td La entrevista es una herramienta esencial para la recolección de datos en investigaciones cualitativas, ya que permite obtener información valiosa directamente de las personas involucradas en el estudio. Esta interacción oral entre el investigador y el sujeto de estudio es fundamental para comprender los aspectos cognitivos, así como las percepciones personales y sociales que influyen en una realidad particular. Existen varias modalidades de entrevistas, cada una con su enfoque y estructura específicos:
+                  p 
+                  li Entrevista asistemática o libre. 
+                  li Entrevista estructurada.
+                  li Entrevista focalizada.
+                  li Entrevista simultánea.
+                  li Entrevista sucesiva.
+              tr
+                td.text-center Encuesta
+                td Son una técnica valiosa para recopilar datos en investigaciones y estudios de opinión. Se basan en la observación y recopilación de información a través de un conjunto de preguntas dirigidas a una muestra representativa de la población de estudio. Estos son los tres tipos principales de encuestas:
+                  p
+                  li Escritas.
+                  li Verbales.
+                  li Mixtas.
+              tr
+                td.text-center Cuestionario
+                td Es un instrumento estructurado diseñado para recopilar datos cuantitativos de manera estandarizada y generalizada. Consiste en un conjunto de preguntas predefinidas que se aplican a todos los informantes de manera uniforme, sin permitir repreguntas directas diferentes. Las respuestas suelen ser de opción múltiple o de elección entre respuestas predefinidas. Los cuestionarios son valiosos en la investigación cuantitativa para recopilar datos eficientemente de un gran número de participantes, pero pueden limitar la profundidad de las respuestas y la exploración en detalle de temas. Existen tres tipos principales de cuestionarios:
+                  p
+                  li El cuestionario estructurado o con preguntas cerradas, donde el informante, para cada pregunta, tiene como únicas alternativas de respuesta las que aparecen en el cuestionario.
+                  li El cuestionario no estructurado o con preguntas abiertas, donde el informante, para cada pregunta tiene total libertad de expresar su propia respuesta.
+                  li El cuestionario mixto, que incluye tanto preguntas abiertas, como preguntas cerradas.
 
 </template>
 
@@ -26,6 +255,50 @@ export default {
   name: 'Tema2',
   data: () => ({
     // variables de vue
+    datosSlyder: [
+      {
+        titulo: 'Evaluación diagnóstica',
+        texto:
+          'Evalúa las fortalezas, debilidades, conocimientos y habilidades de un alumno antes de la formación. Sirve para conocer el punto de partida. Es una base desde la cual trabajar los conocimientos que los alumnos van a adquirir durante el programa formativo.',
+        imagen: require('@/assets/curso/temas/tema4/7.jpg'),
+        // leyendaImagen: 'Leyenda de la imagen',
+      },
+      {
+        titulo: 'Evaluación formativa',
+        texto:
+          'Evalúa el rendimiento de un alumno durante la formación y, por lo general, se produce con regularidad durante todo el proceso de instrucción. Es como un repaso para revisar la idoneidad de la formación recibida, por si fuese necesario realizar modificaciones en el método de enseñanza.',
+        imagen: require('@/assets/curso/temas/tema4/8.jpg'),
+        // leyendaImagen: 'Leyenda de la imagen',
+      },
+      {
+        titulo: 'Evaluación sumativa',
+        texto:
+          'Mide el rendimiento de un estudiante al final de la formación. Es la forma de saber qué ha aprendido y qué no.',
+        imagen: require('@/assets/curso/temas/tema4/9.jpg'),
+        // leyendaImagen: 'Leyenda de la imagen',
+      },
+      {
+        titulo: 'Evaluación por medias',
+        texto:
+          'Compara el desempeño de un estudiante con el de otros compañeros/as. Es una especie de evaluación grupal o “demográfica” que ayuda al docente a obtener medias y sacar conclusiones generales.',
+        imagen: require('@/assets/curso/temas/tema4/10.jpg'),
+        // leyendaImagen: 'Leyenda de la imagen',
+      },
+      {
+        titulo: 'Evaluación basada en objetivos',
+        texto:
+          'Mide el rendimiento de un alumno frente a una meta, un objetivo específico o estándar. Consigue obtener datos y conclusiones más concretas sobre el proceso de enseñanza y aprendizaje.',
+        imagen: require('@/assets/curso/temas/tema4/11.jpg'),
+        // leyendaImagen: 'Leyenda de la imagen',
+      },
+      {
+        titulo: 'Evaluación intermedia o de referencia',
+        texto:
+          'Evalúa el rendimiento del alumno por intervalos de tiempo periódicos. Normalmente coinciden con el final de períodos de calificación: meses, trimestres, etc. Ayuda a predecir el rendimiento de los estudiantes en las evaluaciones sumativas de final de año.',
+        imagen: require('@/assets/curso/temas/tema4/12.jpg'),
+        // leyendaImagen: 'Leyenda de la imagen',
+      },
+    ],
   }),
   mounted() {
     this.$nextTick(() => {
