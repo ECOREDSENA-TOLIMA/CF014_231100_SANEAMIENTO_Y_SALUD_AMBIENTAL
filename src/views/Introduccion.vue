c7da7573f91bbee9f94e3ce2c925f37dc8371735
Sebastian Trujillo <129899570+strujilloa@users.noreply.github.com>
2023-11-17 10:37:12 -0500

    Update tema 1

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -8,6 +8,12 @@
         span
           i.fas.fa-info
       h1 Introducción
+    p(data-aos="fade-left") La sociedad del siglo XXI está preocupada por la sanidad y la salud del ambiente a nivel mundial, por lo que se ha hecho necesario concienciar a la ciudadanía en general sobre la responsabilidad individual y colectiva frente a lo que cada uno puede aportar para que todos podamos cuidar el medio ambiente y dejar a las futuras generaciones un planeta sano y en el cual puedan vivir.
+    p.mb-5(data-aos="fade-left") Por tal razón, se hace necesario que se conozcan algunas herramientas que ayudarán a diseñar, planear, ejecutar, coordinar, hacer seguimiento y evaluación mediante indicadores a procesos de formación comunitaria ambiental que realmente impacte la comunidad y que le permite ver cambios a largo plazo, por lo que le invito a ver el siguiente video:
+
+    figure(data-aos="fade-right")
+      .video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/Dd66jHYKt1o?si=HaBBYMIm5qBGg_aE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
     
 </template>
 
--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -6,15 +6,289 @@
       .titulo-principal__numero
         span 1
       h1 Formatos
+    .row 
+      p(data-aos="fade-left") Por formato se entiende a la manera en la que se organiza la información con respecto a las necesidades específicas de cada persona u organización, esto permite almacenar datos de manera precisa y ordenada, son una guía sobre cómo se debe diligenciar cierta información, que puede ser desde un registro de capacitación hasta una cita médica, sin embargo, en ambos casos se necesitan los datos de las personas involucradas para dejar constancia del proceso y hacer seguimiento si se requiere.
+    .row.justify-content-center.align-items-center 
+      .col-lg-4.col-md-8.col-12.mb-3
+        figure
+          img(src='@/assets/curso/temas/tema1/1.svg', alt='Imagen decorativa')
+      .col-lg-8.col-12.mb-3
+        .cajon.p-4(style="background-color: #C1E29A")
+          .row.justify-content-center
+            .col-12
+              p(data-aos="fade-left") Por lo general, la información que más tiende a tenerse en cuenta dentro de los formatos es la que se lista a continuación, sin embargo, vale la pena aclarar que esta varía dependiendo de lo que se esté buscando:
+          .row.justify-content-center
+            .col-5
+              ul.lista-ul
+                li 
+                  i.fas.fa-check(style="color: #FFAE7E")
+                  | Nombre.
+                li 
+                  i.fas.fa-check(style="color: #FFAE7E")
+                  | Documento de identificación.
+                li 
+                  i.fas.fa-check(style="color: #FFAE7E")
+                  | Edad.
+                li 
+                  i.fas.fa-check(style="color: #FFAE7E")
+                  | Género.
+            .col-5 
+              ul.lista-ul
+                li 
+                  i.fas.fa-check(style="color: #FFAE7E")
+                  | Teléfono.
+                li 
+                  i.fas.fa-check(style="color: #FFAE7E")
+                  | Correo electrónico.
+                li 
+                  i.fas.fa-check(style="color: #FFAE7E")
+                  | Fecha de diligenciamiento.
+                li 
+                  i.fas.fa-check(style="color: #FFAE7E")
+                  | Firma.
+
+    .row
+      p(data-aos="fade-left") El formato es el conjunto de técnicas, tamaño, medida y forma que posee un documento en su presentación, ya sea impreso o en formato digital. Estas características no solo definen la manera en que deben presentarse ciertos documentos, sino que también hacen referencia al tamaño de la impresión.
+      p(data-aos="fade-left") Este término también se aplica a productos audiovisuales y de software, los cuales varían según los medios en los que se vayan a difundir. A continuación, se presentarán algunos de estos formatos utilizados en multimedia y en la producción de documentos, que podrían ser de utilidad al desempeñar el rol de promotor de saneamiento y salud ambiental en cualquier parte del país:
+
+    .row
+      .tarjeta.p-4(style="background-color: #EEFFDE")
+        PasosB.color-acento-contenido
+
+          .row.justify-content-center.col-ipad.align-items-center(titulo="")
+            .col-md-12.mb-4.mb-md-0.col-lg-5
+              h4 Formatos de video
+              p #[b Wmv:] para la compresión de archivos desarrollados con las tecnologías de Microsoft.
+              p #[b Avi:] para archivos pesados.
+              p #[b Mpeg:] para videos digitales que vayan a ser transmitidos en televisión de alta definición.
+              p #[b Mov:] para archivos desarrollados con la tecnología de Apple.
+          
+            .col-md-10.col-lg-5
+              figure
+                img(src='@/assets/curso/temas/tema1/2.jpg', alt='Imagen decorativa')
+
+          .row.justify-content-center.col-ipad.align-items-center(titulo="")
+            .col-md-10.mb-4.mb-md-0.col-lg-5
+              h4 Formatos de compresión
+              p #[b  Zip:] este es el formato más utilizado para comprimir archivos sin que se pierdan sus datos. 
+              p Puede comprimir cualquier tipo de archivo, ya sea videos, imágenes, textos o audios. 
+          
+            .col-md-12.col-lg-5
+              figure
+                img(src='@/assets/curso/temas/tema1/3.jpg', alt='Imagen decorativa')
+
+          .row.justify-content-center.col-ipad.align-items-center(titulo="")
+            .col-md-10.mb-4.mb-md-0.col-lg-5
+              h4 Formatos de audio
+              p #[b Wma:] se utiliza para audios que sean difundidos en internet.
+              p #[b Wav:] es utilizado para aquellos que le den manejo en Windows.
+              p #[b Mp3:] este es el formato de audio más utilizado, ya que se puede comprimir en archivos pequeños similares a los de un CD.
+          
+            .col-md-12.col-lg-5
+              figure
+                img(src='@/assets/curso/temas/tema1/4.jpg', alt='Imagen decorativa')
+
+          .row.justify-content-center.col-ipad.align-items-center(titulo="")
+            .col-md-10.mb-4.mb-md-0.col-lg-5
+              h4 Formatos de uso exclusivo
+              p #[b Aep:] estos formatos están diseñados para detectar aquellos archivos que se utilizarán únicamente en Adobe After Effects.
+              p #[b Prproj:] son los que identifican los archivos que solo se usarán en Adobe Premiere.
+              p #[b Psd:] estos formatos son utilizados únicamente por Adobe Photoshop.
+              p #[b Cdr:] son los archivos que solo implementa el Corel Draw.
+          
+            .col-md-12.col-lg-5
+              figure
+                img(src='@/assets/curso/temas/tema1/5.jpg', alt='Imagen decorativa')
+
+          .row.justify-content-center.col-ipad.align-items-center(titulo="")
+            .col-md-10.mb-4.mb-md-0.col-lg-5
+              h4 Formatos de impresos
+              p Es el tamaño de los materiales impresos en los que se cuenta tanto el ancho como el largo de las hojas. Además, comprenden los espacios que se tienen con respecto a las márgenes. 
+          
+            .col-md-12.col-lg-5
+              figure
+                img(src='@/assets/curso/temas/tema1/6.jpg', alt='Imagen decorativa')
+
+          .row.justify-content-center.col-ipad.align-items-center(titulo="")
+            .col-md-10.mb-4.mb-md-0.col-lg-5
+              h4 Formatos de hojas de cálculo
+              p #[b Csv:] este formato se utiliza para almacenar archivos en los cuales no se guardan caracteres del programa en el que fue realizado. Esto le permite al usuario abrir la información desde otro tipo de programas.
+              p #[b Xls:] es el formato con el cual se guardan todos los documentos realizados directamente en #[i Microsoft Excel].
+          
+            .col-md-12.col-lg-5
+              figure
+                img(src='@/assets/curso/temas/tema1/7.jpg', alt='Imagen decorativa')
+
+          .row.justify-content-center.col-ipad.align-items-center(titulo="")
+            .col-md-10.mb-4.mb-md-0.col-lg-5
+              h4 Formatos de imagen
+              p #[b Mp:] acá se almacenan aquellos archivos que se abrirán en #[i Paint Brush].
+              p #[b Gif:] este formato es empleado para almacenar imágenes que cuentan con diseños sencillos los cuales integran hasta 256 colores. 
+              p #[b Png:] estos formatos son los más empleados en redes, ya que cuentan con unos archivos más completos.
+              p #[b Jpg o Jpeg:] es utilizado para presentar y almacenar imágenes y fotos que no tienen movimiento. 
+          
+            .col-md-12.col-lg-5
+              figure
+                img(src='@/assets/curso/temas/tema1/8.jpg', alt='Imagen decorativa')
+
+          .row.justify-content-center.col-ipad.align-items-center(titulo="")
+            .col-md-10.mb-4.mb-md-0.col-lg-5
+              h4 Formatos de textos
+              p #[b Rtf:] este es un lenguaje creado por #[i Microsoft] el cual es descriptivo y los programas pueden intercambiar datos.
+              p #[b Txt:] son aquellos formatos que únicamente almacenan texto.
+          
+            .col-md-12.col-lg-5
+              figure
+                img(src='@/assets/curso/temas/tema1/9.jpg', alt='Imagen decorativa')
+
+          .row.justify-content-center.col-ipad.align-items-center(titulo="")
+            .col-md-10.mb-4.mb-md-0.col-lg-5
+              h4 Formatos de textos
+              p #[b Pot:] es un formato en el cual se almacenan únicamente plantillas y diapositivas que fueron creadas en #[i PowerPoint.]
+              p #[b Ppt:] es el formato donde únicamente se guardan las presentaciones, como: imágenes, textos, videos y audios, hechos en #[i PowerPoint].
+          
+            .col-md-12.col-lg-5
+              figure
+                img(src='@/assets/curso/temas/tema1/10.jpg', alt='Imagen decorativa')
 
     separador 
     .titulo-segundo.color-acento-contenido
       h2#t_1_1 1.1. Instrumentos de evaluación
+    .row
+      p(data-aos="fade-left") 
+    .row.justify-content-center.align-items-center
+      .col-lg-4.col-md-8.col-12.mb-3
+        figure
+          img(src='@/assets/curso/temas/tema1/11.png', alt='Imagen decorativa')
+      .col-lg-8.col-12.mb-3
+        .cajon-b.color-acento-contenido.p-3
+          p.mb-0 Se puede definir como una herramienta pedagógica que facilita la detección de los niveles de adquisición de competencias por parte de los estudiantes, así como el logro de los objetivos por parte de los instructores, con el fin de establecer un proceso de mejora continua. A través de este proceso, es posible diseñar un plan de refuerzo que contribuya a alcanzar las metas previstas al final del proceso de enseñanza-aprendizaje.
+            br
+            br
+            | La utilización de instrumentos de evaluación en el proceso de enseñanza-aprendizaje conlleva varias ventajas, que, cuando se aplican adecuadamente, pueden propiciar el éxito en el trabajo con el grupo. Algunas de estas ventajas incluyen:
+    .row.justify-content-center.mt-5
+      .col-lg-10.col-12
+        .cajon.p-3.mb-3(style="background-color: #EEFFDE")
+          .row.justify-content-center 
+            .col-lg-11
+              p Cuando se utilizan los instrumentos de evaluación estos permiten detectar necesidades educativas, individuales o grupales, lo que llevará a diseñar un plan de mejora continua para que los aprendices alcancen los resultados esperados.
+        .cajon.p-3.mb-3(style="background-color: #EEFFDE")
+          .row.justify-content-center 
+            .col-lg-11
+              p Al aplicar los instrumentos de evaluación y ver los resultados obtenidos, estos permiten observar si hay necesidad de reforzar algún contenido, objetivo o competencia; sí el problema es en una parte del grupo o si se presenta en un porcentaje más grande, lo que le lleva a definir un plan de acción puntual que fortalezca el aprendizaje colectivo.
+        .cajon.p-3.mb-3(style="background-color: #EEFFDE")
+          .row.justify-content-center 
+            .col-lg-11
+              p Cuando se tienen los resultados de los instrumentos de evaluación, se podrá identificar cuáles aprendices requieren apoyo individual por sus características propias o por las dificultades de aprendizaje que presenta, por lo que puede generar un plan de aprendizaje personalizado fortaleciendo los diferentes procesos de enseñanza y aprendizaje.
+        .cajon.p-3.mb-3(style="background-color: #EEFFDE")
+          .row.justify-content-center 
+            .col-lg-11
+              p Mediante los resultados de la evaluación se podrá medir el grado de cumplimiento de objetivos y competencias, partiendo siempre del plan propuesto de aprendizaje esperado al inicio del proceso.
+        .cajon.p-3.mb-3(style="background-color: #EEFFDE")
+          .row.justify-content-center 
+            .col-lg-11
+              p Al revisar los resultados de la evaluación se podrá identificar cómo ha sido el proceso de enseñanza de los instructores, lo cual se ve directamente reflejado en la calidad de respuestas del alumno, ya que es allí donde este demuestra si este aprendió o no.
+        .cajon.p-3.mb-3(style="background-color: #EEFFDE")
+          .row.justify-content-center 
+            .col-lg-11
+              p Teniendo los resultados de los instrumentos de evaluación, se podrá crear un proceso de mejora continua para reforzar la enseñanza y el aprendizaje, o diseñar proyectos con otros instructores para fortalecer las competencias desde varias aristas.
+        .cajon.p-3.mb-3(style="background-color: #EEFFDE")
+          .row.justify-content-center 
+            .col-lg-11
+              p Al revisar los resultados de las evaluaciones se identificará cuáles tipos de aprendizaje se dieron, cuáles herramientas y métodos se usaron, etc., que impactaron más al aprendiz o al grupo de aprendices para replicar la experiencia o cambiarla si es necesario.
+
+    .row
+      p(data-aos="fade-left") Además, en el proceso de enseñanza y aprendizaje, es esencial abordar otros elementos que influyen directamente. Uno de estos aspectos clave son las diversas técnicas de evaluación de los estudiantes que se emplean en el ámbito educativo. Algunas de las técnicas que se pueden mencionar incluyen:
+
+    .row.justify-content-center.mb-5
+      .col-xl-3.col-lg-4.col-md-9.col-11.mb-4.mb-xl-0
+        .crd_hover_txt(data-aos="flip-left")
+          .crd_hover_txt--img
+            figure
+              img(src="@/assets/curso/temas/tema1/12.png", alt="Imagen decorativa")
+          .crd_hover_txt--body.text-small
+            h3.text-center.mb-3 Técnicas de observación
+            p Estas técnicas permiten evidenciar cómo se comportan los alumnos durante la clase, su interacción en trabajos en equipo y su participación en las actividades del aula. La observación directa brinda información precisa sobre el desempeño de los estudiantes y su involucramiento en el proceso de aprendizaje.
+      .col-xl-3.col-lg-4.col-md-9.col-11.mb-4.mb-xl-0
+        .crd_hover_txt(data-aos="flip-left")
+          .crd_hover_txt--img
+            figure
+              img(src="@/assets/curso/temas/tema1/13.png", alt="Imagen decorativa")
+          .crd_hover_txt--body.text-small
+            h3.text-center.mb-3 Técnicas por escrito
+            p Son una herramienta eficaz para mantener un registro detallado de los resultados de los estudiantes. Entre estas técnicas se incluyen los exámenes, trabajos y proyectos, ensayos, resúmenes, cuestionarios, entre otros. Estas evaluaciones escritas ofrecen una visión más profunda de la comprensión y el desempeño de los alumnos en el material de estudio.
+      .col-xl-3.col-lg-4.col-md-9.col-11.mb-4.mb-xl-0
+        .crd_hover_txt(data-aos="flip-left")
+          .crd_hover_txt--img
+            figure
+              img(src="@/assets/curso/temas/tema1/14.png", alt="Imagen decorativa")
+          .crd_hover_txt--body.text-small
+            h3.text-center.mb-3 Técnicas orales
+            p Son útiles para evaluar el entendimiento y la expresión de los temas impartidos por parte de los estudiantes. Estas técnicas incluyen actividades como presentaciones, debates, discusiones en clase, exposiciones, entrevistas, y otros métodos que requieren que los alumnos se comuniquen verbalmente para demostrar su comprensión y capacidad de expresión en el tema estudiado.
+      .col-xl-3.col-lg-4.col-md-9.col-11.mb-4.mb-xl-0
+        .crd_hover_txt(data-aos="flip-left")
+          .crd_hover_txt--img
+            figure
+              img(src="@/assets/curso/temas/tema1/15.png", alt="Imagen decorativa")
+          .crd_hover_txt--body.text-small
+            h3.text-center.mb-3 Técnicas interactivas
+            p Son aquellas que hacen uso de herramientas digitales o de interacción para facilitar el proceso de aprendizaje. Entre las más comunes se encuentran los mapas mentales, los mapas conceptuales, los diagramas de flujo, las presentaciones con diapositivas y los simuladores educativos. Estas técnicas fomentan la participación activa de los estudiantes y les permiten explorar conceptos y contenidos de manera más dinámica y visual.
+
 
     separador 
     .titulo-segundo.color-acento-contenido
       h2#t_1_2 1.2. Listados
-      
+    
+    .row 
+      p(data-aos="fade-left") Los listados, también conocidos como registros, desempeñan un papel crucial en diversos contextos, ya que permiten llevar un seguimiento de la asistencia de las personas en eventos, actividades educativas, reuniones y entornos laborales. Su objetivo principal es facilitar la gestión de la participación, la puntualidad y el aporte de los participantes.
+
+    .row.justify-content-center.align-items-center 
+      .cajon.p-4(style="background-color: #E6FFF9")
+        .row.justify-content-center.align-items-center
+          .col-lg-6.col-12.mb-3
+            p(data-aos="fade-left") En el ámbito educativo, estos listados son esenciales para hacer un seguimiento de la asistencia y el rendimiento de los estudiantes, así como para identificar ausencias y retrasos. La información recopilada a través de estos registros es valiosa para establecer tiempos y entregables en actividades académicas.
+            p(data-aos="fade-left") Además, los listados se utilizan en entornos laborales para llevar un control de la asistencia y la puntualidad de los empleados. También se emplean para recompensar a aquellos que demuestran puntualidad y asistencia consistentes.
+            p(data-aos="fade-left") Para que estos listados cumplan con su propósito, es fundamental que contengan una serie de indicadores específicos, los cuales pueden variar en complejidad según las necesidades del contexto. A continuación, se mencionan algunos de los elementos comunes que suelen incluirse en estos registros:
+            ul.lista-ul
+              li 
+                i.fas.fa-check(style="color: #FFAE7E")
+                | Nombre del instructor, maestro o encargado de impartir la capacitación.
+              li 
+                i.fas.fa-check(style="color: #FFAE7E")
+                | Nombre del curso, evento o materias que se impartirá.
+              li 
+                i.fas.fa-check(style="color: #FFAE7E")
+                | Periodo en el cual se llevará a cabo la capacitación.
+              li 
+                i.fas.fa-check(style="color: #FFAE7E")
+                | Fechas de inicio y finalización.
+              li 
+                i.fas.fa-check(style="color: #FFAE7E")
+                | Horas de inicio y de finalización.
+              li 
+                i.fas.fa-check(style="color: #FFAE7E")
+                | Nombre, número de cédula y firma de los asistentes.
+              li 
+                i.fas.fa-check(style="color: #FFAE7E")
+                | Nombre de la empresa o ubicación del asistente.
+              li 
+                i.fas.fa-check(style="color: #FFAE7E")
+                | Nombre de la institución o empresa que ejecuta el trabajo.
+              li 
+                i.fas.fa-check(style="color: #FFAE7E")
+                | Logos de la empresa que ejecuta el trabajo.
+              li 
+                i.fas.fa-check(style="color: #FFAE7E")
+                | Datos de contacto de los asistentes, correo electrónico o número de teléfono.
+              li 
+                i.fas.fa-check(style="color: #FFAE7E")
+                | Firma del instructor.
+              li 
+                i.fas.fa-check(style="color: #FFAE7E")
+                | En algunos casos se anexan fotos o videos como evidencias de la ejecución del proceso, según se requiera.
+          .col-lg-4.col-md-8.col-12.mb-3
+            figure
+              img(src="@/assets/curso/temas/tema1/16.png", alt="Imagen decorativa")
 </template>
 
 <script>
